4147f660e7ec4121c223dc2c1bd2ae2a5759f7d4
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2024-02-27 11:08:05 -0500

    connections

--- app.py
+++ app.py
@@ -13,13 +13,15 @@ import psycopg2
 # conn = psycopg2.connect(url_object)
 # print("Connection established")
 
-database_uri = "postgres://mpzjqsja:2s8dz_Ae5V1hNjWUDIFYl8KQ8n20XJUK@stampy.db.elephantsql.com/mpzjqsja"
+# database_uri = "postgres://mpzjqsja:2s8dz_Ae5V1hNjWUDIFYl8KQ8n20XJUK@stampy.db.elephantsql.com/mpzjqsja"
 
 # from sqlalchemy import create_engine
 
 # engine = create_engine('postgresql+psycopg2://postgres:Getfuzzy1@127.0.0.1:5432/blogly')
 
 # url_object = psycopg2.connect(user="postgres", password="Getfuzzy@1", host="azure-postgres1.postgres.database.azure.com", port=5432, database="postgres")
+
+database_uri = psycopg2.connect(user="postgres", password="Getfuzzy@1", host="cloudpostgres.postgres.database.azure.com", port=5432, database="postgres")
 
 from sqlalchemy import URL
 
