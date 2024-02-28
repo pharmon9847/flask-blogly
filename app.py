70dfbdbc567c406d088b81704b0d3423cc2c7b8c
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2024-02-28 11:27:11 -0500

    yup

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ import os
 # conn = psycopg2.connect(url_object)
 # print("Connection established")
 
-# database_uri = "postgres://mpzjqsja:2s8dz_Ae5V1hNjWUDIFYl8KQ8n20XJUK@stampy.db.elephantsql.com/mpzjqsja"
+database_uri = "postgres+psycopg2://mpzjqsja:2s8dz_Ae5V1hNjWUDIFYl8KQ8n20XJUK@stampy.db.elephantsql.com/mpzjqsja"
 
-from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
 
-engine = create_engine('postgresql+psycopg2://mpzjqsja:2s8dz_Ae5V1hNjWUDIFYl8KQ8n20XJUK@stampy.db.elephantsql.com/mpzjqsja')
+# engine = create_engine('postgresql+psycopg2://mpzjqsja:2s8dz_Ae5V1hNjWUDIFYl8KQ8n20XJUK@stampy.db.elephantsql.com/mpzjqsja')
 
 # url_object = psycopg2.connect(user="postgres", password="Getfuzzy@1", host="azure-db1.postgres.database.azure.com", port=5432, database="postgres")
 
@@ -59,8 +59,8 @@ app = Flask(__name__)
 # database_uri = 'postgres://tzhjbumr:7LhS3yvsI2BcGdKfYVaMN6X-iYuro_1M@stampy.db.elephantsql.com/tzhjbumr'
 # database_uri = 'psycopg2.connect(user="postgres", password="Getfuzzy1", host="azure-postgres1.postgres.database.azure.com", port=5432, database="postgres")'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://mpzjqsja:W6DGKwNStfs0uhMwOG8oMozbdZEwo6u9@stampy.db.elephantsql.com/mpzjqsja"
-# app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://mpzjqsja:W6DGKwNStfs0uhMwOG8oMozbdZEwo6u9@stampy.db.elephantsql.com/mpzjqsja"
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'ihaveasecret'
 
