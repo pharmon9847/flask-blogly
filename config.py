cd1cb365bcea255749988ca752694f82e470a9fe
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2024-02-28 10:47:28 -0500

    still more

--- config.py
+++ config.py
@@ -1,14 +1,29 @@
+# class Config:
+#     DEBUG = False
+#  DEVELOPMENT = False
+#  CSRF_ENABLED = True
+#  ASSETS_DEBUG = False
+
+# class ProductionConfig(Config):
+#  pass
+
+# class DevelopmentConfig(Config): 
+#  DEBUG = True
+#  DEVELOPMENT = True
+#  TEMPLATES_AUTO_RELOAD = True
+#  ASSETS_DEBUG = True
+
 class Config:
-     DEBUG = False
- DEVELOPMENT = False
- CSRF_ENABLED = True
- ASSETS_DEBUG = False
+    DEBUG = False
+    DEVELOPMENT = False
+    CSRF_ENABLED = True
+    ASSETS_DEBUG = False
+    
+class ProductionConfig(Config)
+    pass
 
-class ProductionConfig(Config):
- pass
-
-class DevelopmentConfig(Config): 
- DEBUG = True
- DEVELOPMENT = True
- TEMPLATES_AUTO_RELOAD = True
- ASSETS_DEBUG = True
+class DevelopmentConfig(Config)
+    DEBUG = True
+    DEVELOPMENT = True
+    TEMPLATES_AUTO_RELOAD = True
+    ASSETS_DEBUG = True
